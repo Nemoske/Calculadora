f7a7a13983ee0079b2cb3f437833b68c66a097d1
Nemoske <luandevfreitas7@gmail.com>
2024-06-15 20:32:01 -0300

    Projeto Finalizado

--- src/script.js
+++ src/script.js
@@ -4,6 +4,81 @@
 //ESCREVA SEU CÓDIGO AQUI!!
 
 
+//criar uma calculadora que faça as operações: soma, subtração, divisão, multiplicação e exponenciação.
+  let num1;
+  let num2;
+  let operador;
+  let resultado
+
+function Calculadora(numero1, numero2, operacao){
+
+  num1 = parseFloat(numero1);
+  num2 = parseFloat(numero2);
+  operador = operacao;
+  resultado = 1;  
+
+  if(operador == "+" || operador == "-" || operador == "/" || operador == "*" || operador == "e" ){
+
+    if(operador == "+"){
+      resultado = parseFloat(num1 + num2);
+      if(resultado === 'undefined' || resultado > 1000000){
+        return resultado = "ERRO"
+      }
+      else{
+        return resultado;
+      }
+      
+    }
+
+    else if(operador == "-"){
+      resultado = num1 - num2;
+      if(resultado == undefined || resultado > 1000000){
+        return resultado = "ERRO"
+      }
+      else{
+        return resultado;
+      }
+    }
+
+    else if(operador == "/"){
+      resultado = num1 / num2;
+      if(resultado == undefined || resultado > 1000000){
+        return resultado = "ERRO"
+      }
+      else{
+        return resultado;
+      }
+    }
+    
+    else if(operador == "*"){
+      resultado = num1 * num2;
+      if(resultado == undefined || resultado > 1000000){
+        return resultado = "ERRO"
+      }
+      else{
+        return resultado;
+      }
+    }
+
+    else if(operador == "e"){
+        for(let i = 0; i < num2; i++ ){
+          resultado *= num1
+        }
+        
+        if(resultado == undefined || resultado > 1000000){
+          return resultado = "ERRO"
+        }
+        else{
+          return resultado;
+        }
+    }
+
+
+
+
+  } 
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -18,13 +93,16 @@ function limpezatotal() {
 }
 
 function calcular() {
-  let numeros = visor.value.split(/[^0-9]/)
-  let operadores = visor.value.split(/[0-9]/)
-  let operador
-  operadores.forEach(x => {
-    if (x != "")
-      operador = x
-  });
+  let expressao = visor.value;
+  let numeros = expressao.split(/([+\-*/e])/).filter(function(e) { return e; });
+  let numero1 = parseFloat(numeros[0]);
+  let operador = numeros[1];
+  let numero2 = parseFloat(numeros[2]);
 
-  visor.value = Calculadora(numeros[0], numeros[1], operador)
+  if (!isNaN(numero1) && !isNaN(numero2) && operador) {
+    visor.value = Calculadora(numero1, numero2, operador);
+  } else {
+    visor.value = "ERRO";
+  }
 }
+
